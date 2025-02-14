7662c8e0b3b9a9def8c67aeba834a03acbbc9745
Vladimir Riabchun <ferr.lambarginio@gmail.com>
2025-02-14 23:55:29 +0100

    Split clang compilation and opt execution in tests. Now it is possible to test IR files without C sources

--- tests/test.sh
+++ tests/test.sh
@@ -6,7 +6,11 @@ mkdir "$IR_PATH" "$RESULT"
 PASS="$1"
 for file in "$C_SRC"/*.c; do
     FILE_NAME="$(basename "$file" .c)"
-    clang-14 -S -emit-llvm -o "$IR_PATH/$FILE_NAME.ll" "$file" &&
-    opt-14 -enable-new-pm=0 -load="$PASS" -instr -S -o "$RESULT/$FILE_NAME.ll" "$IR_PATH/$FILE_NAME.ll"
+    clang-14 -S -emit-llvm -o "$IR_PATH/$FILE_NAME.ll" "$file"
+done
+for file in "$IR_PATH"/*.ll; do
+    FILE_NAME="$(basename "$file")"
+    opt-14 -enable-new-pm=0 -load="$PASS" -instr -S -o "$RESULT/$FILE_NAME" "$IR_PATH/$FILE_NAME"
     echo
 done
+
