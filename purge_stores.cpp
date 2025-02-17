90293f01843682bfdc71ecb445a1ece8362a2038
Vladimir Riabchun <ferr.lambarginio@gmail.com>
2025-02-17 16:13:48 +0100

    Implement basic store remover

---
+++ purge_stores.cpp
@@ -0,0 +1,77 @@
+#include <unordered_set>
+#include "llvm/Pass.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include "util.h"
+
+using namespace llvm;
+
+// TODO: read list of structures from a file
+std::unordered_set<std::string> NO_PTR_STORE = {
+        "struct.list_head",
+        "struct.hlist_node",
+        "struct.llist_node",
+};
+
+struct StorePurgerPass : public ModulePass {
+    StorePurgerPass(): ModulePass(ID) {}
+
+    bool runOnModule(Module &M) override {
+        findAllBadStructs(M);
+        removeAllStores(M);
+        return false;
+    }
+    static char ID;
+private:
+    std::unordered_set<StructType*> restricted_for_store;
+
+    void findAllBadStructs(Module &M) {
+        for (auto s : M.getIdentifiedStructTypes()) {
+            if (NO_PTR_STORE.contains(s->getName().operator std::string())) {
+                restricted_for_store.insert(s);
+                outs() << "Found matching struct " << s->getName() << "\n";
+            }
+        }
+        outs().flush();
+    }
+
+    void removeAllStores(Module &M) {
+        size_t purged = 0;
+        for (auto &f : M) {
+            for (auto &bb : f) {
+                std::vector<Instruction*> remove_list;
+                for (auto &inst : bb) {
+                    if (auto *store = dyn_cast<StoreInst>(&inst)) {
+                        auto *type = dereferenceStructPtr(store->getValueOperand()->getType());
+                        if (!type || !restricted_for_store.contains(type)) {
+                            continue;
+                        }
+                        purged++;
+                        remove_list.push_back(store);
+                        //store->eraseFromParent();
+                    }
+                }
+                for (auto i : remove_list) {
+                    i->eraseFromParent();
+                }
+            }
+        }
+        outs() << "Removed " << purged << " stores\n";
+        outs().flush();
+    }
+};
+
+char StorePurgerPass::ID = 0;
+
+static RegisterPass<StorePurgerPass> X("remove-store", "Remove unwanted stores",
+                                         false /* Only looks at CFG */,
+                                         false /* Analysis Pass */);
+
+static RegisterStandardPasses Y(
+        PassManagerBuilder::EP_OptimizerLast,
+        [](const PassManagerBuilder &Builder, legacy::PassManagerBase &PM) {
+            PM.add(new StorePurgerPass());
+        }
+);
